ea3d169f186583eaea6d0992b8870b3736972d61
Steven Suzuki <suzukisteven@gmail.com>
2018-10-22 17:59:21 +0800

    6th commit: refactored a bit

--- models/question.rb
+++ models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 5 }, uniqueness: true
   validates :description, presence: true, length: { minimum: 5 }
