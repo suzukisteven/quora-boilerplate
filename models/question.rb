6ff7a1b568208b44934cb1e377e70f0d8aabe601
Steven Suzuki <suzukisteven@gmail.com>
2018-10-22 10:36:10 +0800

    third commit: about to start answers

--- models/question.rb
+++ models/question.rb
@@ -1,5 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+  has_many :answers
+  
   validates :title, presence: true, length: { minimum: 5 }, uniqueness: true
-  validates :description, presence: true, length: { minimum: 5 }, uniqueness: true
+  validates :content, presence: true, length: { minimum: 5 }
 end
--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :questions
+  has_many :answers
 
   validates :username, presence: true, length: { minimum: 6 }, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
